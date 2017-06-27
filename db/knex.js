32d8d49475259db028fe9d7e0ab23180e9a177a2
Patrick Weaver <pjpwea@gmail.com>
2017-06-27 09:20:07 -0400

    reset for flask

--- db/knex.js
+++
@@ -1,9 +0,0 @@
-var environment = process.env.NODE_ENV;
-if (!environment) {
-  environment = "development";
-}
-console.log("**");
-console.log(environment);
-const config = require('../knexfile.js')[environment];
-console.log(config);
-module.exports = require('knex')(config);
