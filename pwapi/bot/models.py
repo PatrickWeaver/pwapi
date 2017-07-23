92d0673fa7f0736e1b45c8f29d513c2a50c8ac91
Patrick Weaver <pjpwea@gmail.com>
2017-07-23 15:20:39 -0400

    Start setting up bot models

--- pwapi/bot/models.py
+++ pwapi/bot/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# Create your models here.
+class Guest(models.Model):
+    name = models.CharField(max_length=1024)
+
+
+class IPAddress(models.Model):
+    address = models.CharField(max_length=128)
+
+# Which of these systems?
+# https://docs.djangoproject.com/en/1.11/topics/db/models/#intermediary-manytomany
+# https://docs.djangoproject.com/en/1.11/topics/db/examples/many_to_many/
