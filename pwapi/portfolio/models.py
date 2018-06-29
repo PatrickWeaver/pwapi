e01c042e18b0c0317c75833d0c360e7d208ceda2
Patrick Weaver <pjpwea@gmail.com>
2018-06-29 15:07:09 -0400

    fix typo

--- pwapi/portfolio/models.py
+++ pwapi/portfolio/models.py
@@ -25,7 +25,7 @@ class Project(models.Model):
     description = models.TextField(default="")
     start_date = models.DateTimeField()
     end_date = models.DateTimeField()
-    status = models.ForeignKey(Tag, related_name="project_status" on_delete=models.PROTECT)
+    status = models.ForeignKey(Tag, related_name="project_status", on_delete=models.PROTECT)
     tags = models.ManyToManyField(Tag, related_name="project_tags")
     project_url = models.CharField(max_length=1024)
     source_url = models.CharField(max_length=1024)
