4f8d5aeeb1213d5b30b3f0255a5842c49a263087
Glitch (pwapi) <none>
2019-04-18 13:55:24 +0000

    prod script for now

--- start.sh
+++ start.sh
@@ -14,13 +14,13 @@ pip3 install -r requirements.txt --user
 # Python buffers stdout. Without this, you won't see what you "print" in the Activity Logs
 export PYTHONUNBUFFERED=true
 
-cd pwapi
-python3 manage.py runserver
-
-
-# Start Gunicorn processes
-#echo Starting Gunicorn.
 #cd pwapi
-#exec gunicorn pwapi.wsgi:application \
-#    --bind 0.0.0.0:8000 \
-#    --workers 3
+#python3 manage.py runserver
+
+
+Start Gunicorn processes
+echo Starting Gunicorn.
+cd pwapi
+exec gunicorn pwapi.wsgi:application \
+    --bind 0.0.0.0:8000 \
+    --workers 3
